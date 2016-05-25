5076ffd72deb38b23851324646d1eed0a68ff3cc
Tronyc <eyerisgames@gmail.com>
2016-05-24 20:14:54 -0700

    Fixed Typo in MainMenuController.cs

--- Assets/Scripts/GameControllers/MainMenuController.cs
+++ Assets/Scripts/GameControllers/MainMenuController.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.UI;
 
-public class MainMenuContorller : MonoBehaviour {
+public class MainMenuController : MonoBehaviour {
 
 	[SerializeField]
 	private Button musicBtn;
